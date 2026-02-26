e3c1a1ef1823563a8d096dfdbee4458e0701f300
nawf <wahyuyahaha111@gmail.com>
2026-02-26 12:42:39 +0700

    add input.py

---
+++ src/variable-tipedata.py
@@ -0,0 +1,9 @@
+contoh_string = "hallo naw"
+contoh_angka_int = 10
+contoh_boolean = True
+contoh_angka_float = 10.5
+
+print(contoh_string)
+print(contoh_angka_int)
+print(contoh_boolean)
+print(contoh_angka_float)
