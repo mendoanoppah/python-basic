5a573a966ce76192b695216393a08c9c0fb25aba
nawf <wahyuyahaha111@gmail.com>
2026-02-26 13:07:40 +0700

    delete variable-tipedata.py

--- src/variable-tipedata.py
+++
@@ -1,9 +0,0 @@
-contoh_string = "hallo naw"
-contoh_angka_int = 10
-contoh_boolean = True
-contoh_angka_float = 10.5
-
-print(contoh_string)
-print(contoh_angka_int)
-print(contoh_boolean)
-print(contoh_angka_float)
