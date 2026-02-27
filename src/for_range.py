49cf286243a29ac4e2358f75501131803c7fe64e
nawf <wahyuyahaha111@gmail.com>
2026-02-27 23:59:36 +0700

    add for-range.py

---
+++ src/for_range.py
@@ -0,0 +1,45 @@
+print("\n")
+print("FOR LOOP")
+for i in range(5):
+  print(i) # 0-4
+
+print("\n")
+
+for i in range(1, 6):
+  print(i) # 1-5
+
+print("\n")
+
+for i in range(3):
+  print("Hello World") # Hello World 3x
+
+print("\n")
+
+for i in range(5, 0, -1):
+  print(i) # 5-4-3-2-1
+
+print("\n")
+
+kata = str(input("Masukan kata: "))
+print("Tiap huruf-huruf dalam kata: ")
+
+for i in kata:
+  print(i)
+  
+# print("\n")
+
+# for index, huruf in enumerate(nama):
+#   print(index, huruf)
+
+# for huruf in "Python":
+#   print(huruf)
+
+# dictionary
+# data ={"nama": "ucup", "umur": 99}
+# for key, value in data.items():
+#   print(key, value)
+
+# list
+# buah = ["apel", "mangga", "manggis"]
+# for i in buah:
+#   print(i)
