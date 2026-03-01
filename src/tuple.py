7ee6d766e06f13999caef73c4716b0989b7be557
nawf <wahyuyahaha111@gmail.com>
2026-03-01 15:53:45 +0700

    add tuple.py

---
+++ src/tuple.py
@@ -0,0 +1,15 @@
+# data tidak bisa diubah dengan tuple
+numbers = (95, 10, 100, 123, 1222, 41)
+print(numbers)
+print(numbers[0])
+print(numbers[1])
+
+birth_date = (9, 3, 1999)
+print(birth_date)
+
+for t in numbers:
+  print(t)
+
+for i in range(len(numbers)):
+  print(numbers[i])
+  
